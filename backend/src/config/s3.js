7e3f3ba9e5e7f80565d948342297f4203c01de8c
Krishna Rao <krishna638738@gmail.com>
2026-08-12 19:12:42 +0530

    s3 config

--- backend/src/config/s3.js
+++ backend/src/config/s3.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+// config/s3.js
+
+import {
+    S3Client
+} from "@aws-sdk/client-s3";
+
+export const s3 = new S3Client({
+    region: process.env.AWS_REGION
+});
--- backend/src/services/s3.service.js
+++ backend/src/services/s3.service.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+// services/s3.service.js
+
+import {
+    PutObjectCommand
+} from "@aws-sdk/client-s3";
+
+import {
+    getSignedUrl
+} from "@aws-sdk/s3-request-presigner";
+
+import { s3 } from "../config/s3.js";
+
+export async function generateUploadUrl(
+    key,
+    contentType
+) {
+
+    const command = new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET,
+        Key: key,
+        ContentType: contentType
+    });
+
+    return await getSignedUrl(
+        s3,
+        command,
+        {
+            expiresIn: 300
+        }
+    );
+}
